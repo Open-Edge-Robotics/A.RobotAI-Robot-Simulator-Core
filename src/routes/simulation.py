ac1d959bde74024e9aaa6afaef9b87fc04cf7b57
seo-young Yoon <ytjdud01@kookmin.ac.kr>
2024-11-18 15:28:37 +0900

    refactor: [simulation] 기존 응답 구조에 GlobalResponseModel 적용

--- src/routes/simulation.py
+++ src/routes/simulation.py
@@ -1,16 +1,14 @@
-from typing import List
-
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
 from src.crud.simulation import SimulationService
 from src.database.connection import get_db
-from src.schemas.simulation import SimulationCreateModel, SimulationListModel
+from src.schemas.simulation import SimulationCreateModel, SimulationListResponseModel, SimulationCreateResponseModel
 
 simulation_router = APIRouter(prefix="/api/simulation", tags=["simulation"])
 
-@simulation_router.post("/")
+@simulation_router.post("/", response_model= SimulationCreateResponseModel, status_code=201)
 async def create_simulation(
         simulation_create_data: SimulationCreateModel, session: AsyncSession = Depends(get_db)
 ):
@@ -20,7 +18,7 @@ async def create_simulation(
     return new_simulation
 
 
-@simulation_router.get("/", response_model= List[SimulationListModel])
+@simulation_router.get("/", response_model= SimulationListResponseModel, status_code=200)
 async def get_all_simulations(
     session: AsyncSession = Depends(get_db)
 ):
