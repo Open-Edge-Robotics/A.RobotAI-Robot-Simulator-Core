654da74d8242500af65d084a783809dd9d2b2379
seo-young Yoon <ytjdud01@kookmin.ac.kr>
2024-11-18 10:30:18 +0900

    feat: [simulation] 목록 조회 API 구현

--- src/crud/simulation.py
+++ src/crud/simulation.py
@@ -1,8 +1,9 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
 
 from src.models.simulation import Simulation
-from src.schemas.simulation import SimulationCreateModel
+from src.schemas.simulation import SimulationCreateModel, SimulationListModel
 
 
 class SimulationService:
@@ -25,3 +26,23 @@ class SimulationService:
             raise e
 
         return new_simulation
+
+    async def get_all_simulations(self):
+        statement = (
+            select(Simulation).
+            order_by(Simulation.id.desc())
+        )
+        results = await self.session.scalars(statement)
+
+        simulation_list = [
+            SimulationListModel(
+                simulationId=str(simulation.id),
+                simulationName=simulation.name,
+                simulationDescription=simulation.description,
+                simulationCreatedAt=str(simulation.created_at),
+                simulationStatus="RUNNING" # TODO: status 데이터 가져오기
+            )
+            for simulation in results.all()
+        ]
+
+        return simulation_list
