0a6e979b47648afafe3ab4ad23de13a986f4c00e
이노그리드 신이수 <isu010614@naver.com>
2024-11-19 10:08:44 +0900

    feat: template 라우터 추가

--- src/main.py
+++ src/main.py
@@ -1,16 +1,24 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from kubernetes import config
 
 from src.database.connection import init_db, close_db
+from src.routes import template
+from src.settings import settings
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db()
+    # config.load_kube_config('/root/.kube/config')
+
     yield
     await close_db()
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(template.router, prefix=settings.API_STR)
 
 @app.get("/")
 async def root():
