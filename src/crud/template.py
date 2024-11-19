2747c8ddb2b44f11a4f5fafc615c1b62fb879634
이노그리드 신이수 <isu010614@naver.com>
2024-11-19 14:01:26 +0900

    feat: [template] 목록 조회, 생성 기능 구현

---
+++ src/crud/template.py
@@ -0,0 +1,38 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.models.template import Template
+from src.schemas.template import TemplateResponse, TemplateCreate
+
+
+class TemplateService:
+    async def read_all(self, db: AsyncSession):
+        selected_template = await db.execute(select(Template))
+        templates = selected_template.scalars().all()
+
+        return [
+            TemplateResponse(
+                template_id=str(template.template_id),
+                template_type=template.type,
+                template_description=template.description,
+            ) for template in templates
+        ]
+
+    async def create(self, template: TemplateCreate, db: AsyncSession):
+        new_template = Template(
+            type=template.type,
+            description=template.description,
+            bag_file_path=template.bag_file_path,
+            topics=template.topics,
+        )
+        db.add(new_template)
+        await db.commit()
+        await db.refresh(new_template)
+        return new_template
+
+    # async def delete(self, template_id: int, db: AsyncSession):
+    #     find_template = await self.read(template_id, db)
+    #
+    #     await db.delete(find_template)
+    #     await db.commit()
+    #     return find_template
