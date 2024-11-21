0518e3ca1e99d4f98201a1ffa106a05961087b12
seo-young Yoon <ytjdud01@kookmin.ac.kr>
2024-11-21 12:19:45 +0900

    refactor: [simulation] 카멜케이스 적용

--- src/crud/simulation.py
+++ src/crud/simulation.py
@@ -17,13 +17,13 @@ class SimulationService:
             # 시뮬레이션 이름 중복 검사
             statement = select(
                 exists().
-                where(Simulation.name == simulation_create_data.simulationName)
+                where(Simulation.name == simulation_create_data.simulation_name)
             )
             is_existed = await self.session.scalar(statement)
 
             if is_existed:
                 response = SimulationCreateResponseModel(
-                    statusCode=status.HTTP_409_CONFLICT,
+                    status_code=status.HTTP_409_CONFLICT,
                     data=None,
                     message="시뮬레이션 이름이 이미 존재합니다.",
                 )
@@ -31,8 +31,8 @@ class SimulationService:
                 return response
 
             new_simulation = Simulation(
-                name=simulation_create_data.simulationName,
-                description=simulation_create_data.simulationDescription
+                name=simulation_create_data.simulation_name,
+                description=simulation_create_data.simulation_description
             )
 
             self.session.add(new_simulation)
@@ -40,7 +40,7 @@ class SimulationService:
             await self.session.refresh(new_simulation)
 
             response = SimulationCreateResponseModel(
-                statusCode=status.HTTP_201_CREATED,
+                status_code=status.HTTP_201_CREATED,
                 data=None,
                 message="시뮬레이션 생성 성공"
             )
@@ -49,7 +49,7 @@ class SimulationService:
             await self.session.rollback()
 
             response = SimulationCreateResponseModel(
-                statusCode=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 data=None,
                 message="시뮬레이션 생성 실패: " + str(e)
             )
@@ -67,17 +67,17 @@ class SimulationService:
 
             simulation_list = [
                 SimulationListModel(
-                    simulationId=str(simulation.id),
-                    simulationName=simulation.name,
-                    simulationDescription=simulation.description,
-                    simulationCreatedAt=str(simulation.created_at),
-                    simulationStatus="RUNNING" # TODO: status 데이터 가져오기
+                    simulation_id=str(simulation.id),
+                    simulation_name=simulation.name,
+                    simulation_description=simulation.description,
+                    simulation_created_at=str(simulation.created_at),
+                    simulation_status="RUNNING" # TODO: status 데이터 가져오기
                 )
                 for simulation in results.all()
             ]
 
             response = SimulationListResponseModel(
-                statusCode="200",
+                status_code="200",
                 data=simulation_list,
                 message="시뮬레이션 목록 조회 성공"
             )
@@ -85,7 +85,7 @@ class SimulationService:
         except Exception as e:
 
             response = SimulationListResponseModel(
-                statusCode="500",
+                status_code="500",
                 data=None,
                 message="시뮬레이션 목록 조회 실패: " + str(e)
             )
