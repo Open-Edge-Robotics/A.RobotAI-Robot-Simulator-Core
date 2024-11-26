edb6eda22f149218bbbe0fb20c0a8166c576b1a8
이노그리드 신이수 <isu010614@naver.com>
2024-11-26 16:58:11 +0900

    feat: [template] 삭제 라우터 구현

--- src/routes/template.py
+++ src/routes/template.py
@@ -3,12 +3,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from src.crud.template import TemplateService
-from src.database.connection import get_db
+from src.database.db_conn import get_db
 from src.schemas.format import GlobalResponseModel
-from src.schemas.template import TemplateDeleteResponse, TemplateCreateRequest
+from src.schemas.template import TemplateCreateRequest, TemplateDeleteResponse
 
 router = APIRouter(prefix="/template", tags=["Template"])
 template_service = TemplateService()
+
 
 # 템플릿 목록 조회
 @router.get("", response_model=GlobalResponseModel)
@@ -20,12 +21,14 @@ async def get_templates(db: AsyncSession = Depends(get_db)):
         message="템플릿 목록 조회"
     )
 
+
 # 템플릿 생성
 @router.post("", status_code=status.HTTP_201_CREATED)
 async def create_template(template: TemplateCreateRequest, db: AsyncSession = Depends(get_db)):
     return await template_service.create_template(template, db)
 
+
 # 템플릿 삭제
-@router.delete("/{template_id}", response_model=TemplateDeleteResponse, status_code=status.HTTP_200_OK)
+@router.delete("/{template_id}", response_model=TemplateDeleteResponse, status_code=status.HTTP_204_NO_CONTENT)
 async def delete_template(template_id: int, db: AsyncSession = Depends(get_db)):
     return await template_service.delete_template(template_id, db)
