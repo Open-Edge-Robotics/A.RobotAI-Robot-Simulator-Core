e7e555dda2a7bf6863a72b17f475ed626311f366
이노그리드 신이수 <isu010614@naver.com>
2024-11-26 16:54:57 +0900

    chore: 파일명 수정에 따른 import 변경

--- src/models/template.py
+++ src/models/template.py
@@ -4,7 +4,7 @@ from typing import List
 from sqlalchemy import String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.database.connection import Base
+from src.database.db_conn import Base
 from src.models.instance import Instance
 
 
