060d89d888cfca653d63102251770aac3dc18359
이노그리드 신이수 <isu010614@naver.com>
2024-11-26 16:59:51 +0900

    chore: [instance] 컨벤션 정리 및 pod 생성라인 추가

--- src/routes/instance.py
+++ src/routes/instance.py
@@ -3,11 +3,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
 from src.crud.instance import InstanceService
-from src.database.connection import get_db
+from src.database.db_conn import get_db
 from src.schemas.instance import InstanceCreateRequest, InstanceCreateResponseModel, InstanceListResponseModel, \
     InstanceControlRequest, InstanceControlResponseModel, InstanceDeleteResponseModel, InstanceDetailResponseModel
 
 router = APIRouter(prefix="/instance", tags=["Instance"])
+
+
+# instance_service = InstanceService() TODO 변경 필요
 
 @router.post("/", response_model=InstanceCreateResponseModel, status_code=status.HTTP_201_CREATED)
 async def create_instance(
@@ -15,6 +18,7 @@ async def create_instance(
 ):
     """새로운 인스턴스 생성"""
     new_instance = await InstanceService(session).create_instance(instance_create_data)
+    await InstanceService(session).create_pod(1, instance_create_data)
 
     return InstanceCreateResponseModel(
         status_code=status.HTTP_201_CREATED,
@@ -22,18 +26,20 @@ async def create_instance(
         message="인스턴스 생성 성공"
     )
 
+
 @router.get("/", response_model=InstanceListResponseModel, status_code=status.HTTP_200_OK)
 async def get_instances(
-    session: AsyncSession = Depends(get_db)
+        session: AsyncSession = Depends(get_db)
 ):
     """인스턴스 목록 조회"""
-    instance_list= await InstanceService(session).get_all_instances()
+    instance_list = await InstanceService(session).get_all_instances()
 
     return InstanceListResponseModel(
         status_code=status.HTTP_200_OK,
         data=instance_list,
         message="인스턴스 목록 조회 성공"
     )
+
 
 @router.get("/{instance_id}", response_model=InstanceDetailResponseModel, status_code=status.HTTP_200_OK)
 async def get_instance(
@@ -48,6 +54,7 @@ async def get_instance(
         message="인스턴스 상세 조회 성공"
     )
 
+
 @router.post("/action", response_model=InstanceControlResponseModel, status_code=status.HTTP_200_OK)
 async def control_instance(
         instance_control_data: InstanceControlRequest, session: AsyncSession = Depends(get_db)
@@ -60,6 +67,7 @@ async def control_instance(
         data=data,
         message=f"인스턴스 {action} 성공"
     )
+
 
 @router.delete("/{instance_id}", response_model=InstanceDeleteResponseModel, status_code=status.HTTP_200_OK)
 async def delete_instance(
