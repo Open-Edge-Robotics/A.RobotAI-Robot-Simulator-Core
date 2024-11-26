6d03df789ac1cc401f4871b192b77df0a66de286
이노그리드 신이수 <isu010614@naver.com>
2024-11-26 16:52:44 +0900

    feat: MinIO 관련 환경변수 추가

--- src/settings.py
+++ src/settings.py
@@ -1,20 +1,27 @@
 from dotenv import load_dotenv
-from pydantic import BaseModel, PostgresDsn
+from pydantic import BaseModel
 from pydantic.alias_generators import to_camel
 from pydantic_settings import BaseSettings
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
     API_STR: str
     DATABASE_URL: str
+    MINIO_URL: str
+    MINIO_ACCESS_KEY: str
+    MINIO_SECRET_KEY: str
+    MINIO_BUCKET_NAME: str
+
 
 settings = Settings()
 
+
 class BaseSchema(BaseModel):
     class Config:
-        alias_generator=to_camel
-        populate_by_name=True
+        alias_generator = to_camel
+        populate_by_name = True
 
     def model_dump(self):
         return super().model_dump(by_alias=True)
