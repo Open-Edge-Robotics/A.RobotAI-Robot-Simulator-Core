f9f7ca51012d984ca03f527334ccb963f55b5fe0
seo-young Yoon <ytjdud01@kookmin.ac.kr>
2024-11-27 10:15:16 +0900

    refactor: [instance] DB 스키마 변경 및 생성 API 수정

--- src/crud/instance.py
+++ src/crud/instance.py
@@ -1,10 +1,9 @@
 from fastapi import HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import DatabaseError
 from starlette import status
 
-from src.models.instance import Instance, InstanceSet
+from src.models.instance import Instance, Pod
 from src.models.simulation import Simulation
 from src.models.template import Template
 from src.schemas.instance import InstanceCreateRequest, InstanceCreateResponse, InstanceListResponse, \
@@ -16,53 +15,66 @@ class InstanceService:
         self.session = session
 
     async def create_instance(self, instance_create_data: InstanceCreateRequest):
-        # TODO: extract 할 수 있지 않을까? (시뮬id 검사, 템플릿id 검사)
-        # 시뮬레이션 id 검사
-        statement = select(Simulation).where(Simulation.id == instance_create_data.simulation_id)
-        simulation = await self.session.scalar(statement)
+        async with self.session.begin():
+            # TODO: extract 할 수 있지 않을까? (시뮬id 검사, 템플릿id 검사)
+            # 시뮬레이션 id 검사
+            statement = select(Simulation).where(Simulation.id == instance_create_data.simulation_id)
+            simulation = await self.session.scalar(statement)
 
-        if simulation is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='존재하지 않는 시뮬레이션id 입니다.')
+            if simulation is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='존재하지 않는 시뮬레이션id 입니다.')
 
-        # 템플릿 id 검사
-        statement = select(Template).where(Template.template_id == instance_create_data.template_id)
-        template = await self.session.scalar(statement)
 
-        if template is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='존재하지 않는 템플릿id 입니다.')
+            # 템플릿 id 검사
+            statement = select(Template).where(Template.template_id == instance_create_data.template_id)
+            template = await self.session.scalar(statement)
 
-        try:
-            new_instance = Instance(
+            if template is None:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='존재하지 않는 템플릿id 입니다.')
+
+            count = instance_create_data.instance_count
+            new_instances = [
+                Instance(
                 name=instance_create_data.instance_name,
                 description=instance_create_data.instance_description,
-                template_id=instance_create_data.template_id,
+                template_id=template.template_id,
                 template=template,
-            )
-            self.session.add(new_instance)
-            await self.session.commit()
-            await self.session.refresh(new_instance)
+                ) for i in range(count)
+            ]
 
-            new_instances_sets = InstanceSet(
+            self.session.add_all(new_instances)
+
+            await self.session.flush()
+            for new_instance in new_instances:
+                await self.session.refresh(new_instance)
+
+            pod_sets = [
+                Pod(
+                    name = f"instance-{simulation.id}-{new_instance.id}",
                     instance=new_instance,
                     instance_id=new_instance.id,
-                    instance_count=instance_create_data.instance_count,
-                    simulation_id=instance_create_data.simulation_id,
+                    simulation_id=simulation.id,
                     simulation= simulation
-            )
-            self.session.add(new_instances_sets)
-            await self.session.commit()
+                ) for new_instance in new_instances
+            ]
 
-            await self.session.refresh(new_instance)
+            self.session.add_all(pod_sets)
 
-        except DatabaseError as e:
-            await self.session.rollback()
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='데이터 저장 중 오류가 발생했습니다.: ' + str(e))
+            await self.session.flush()
+            for new_pod in pod_sets:
+                await self.session.refresh(new_pod)
 
-        return InstanceCreateResponse(
-            instance_id= new_instance.id,
-            instance_name= new_instance.name,
-            instance_description= new_instance.description
-        ).model_dump()
+            return [
+                InstanceCreateResponse(
+                    instance_id=new_pod.instance_id,
+                    instance_name=new_pod.instance.name,
+                    instance_description=new_pod.instance.description,
+                    template_id=new_pod.instance.template_id,
+                    simulation_id=new_pod.simulation_id,
+                    pod_name=new_pod.name,
+                )
+                for new_pod in pod_sets
+            ]
 
     async def get_all_instances(self):
         try:
