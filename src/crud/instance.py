e4f377f1c75945481cde9945d2bd32b8d244a549
이노그리드 신이수 <isu010614@naver.com>
2024-11-29 11:40:57 +0900

    fix: [instance] 전체 수정
    - 생성
    - 목록 조회
    - 상세 조회

--- src/crud/instance.py
+++ src/crud/instance.py
@@ -1,36 +1,42 @@
-from typing import Optional
+import os
+import subprocess
 
 from fastapi import HTTPException
-from kubernetes import client
+from minio import S3Error
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 from starlette import status
 
-from src.crud.template import TemplateService
+from src.crud.pod import PodService
 from src.crud.simulation import SimulationService
+from src.crud.template import TemplateService
+from src.database import minio_conn
 from src.models.instance import Instance
-from src.models.template import Template
 from src.schemas.instance import InstanceCreateRequest, InstanceCreateResponse, InstanceListResponse, \
-    InstanceControlRequest, InstanceDetailResponse, InstanceControlResponse, InstanceDeleteResponse
+    InstanceDetailResponse, InstanceDeleteResponse, InstanceListRequest
 
-template_service = TemplateService()
+pod_service = PodService()
+
 
 class InstanceService:
     def __init__(self, session: AsyncSession):
         self.session = session
         self.simulation_service = SimulationService(session)
+        self.templates_service = TemplateService(session)
 
     async def create_instance(self, instance_create_data: InstanceCreateRequest):
-        async with self.session.begin():
-            simulation = await self.simulation_service.find_simulation_by_id(instance_create_data.simulation_id, "인스턴스 생성")
-            template = await template_service.find_template_by_id(instance_create_data.template_id, "인스턴스 생성", self.session)
+        async with (self.session.begin()):
+            simulation = await self.simulation_service.find_simulation_by_id(instance_create_data.simulation_id,
+                                                                             "인스턴스 생성")
+            template = await self.templates_service.find_template_by_id(instance_create_data.template_id, "인스턴스 생성")
 
             count = instance_create_data.instance_count
             new_instances = [
                 Instance(
                     name=instance_create_data.instance_name,
                     description=instance_create_data.instance_description,
+                    pod_namespace=instance_create_data.pod_namespace,
                     template_id=template.template_id,
                     template=template,
                     simulation_id=simulation.id,
@@ -40,10 +46,10 @@ class InstanceService:
             self.session.add_all(new_instances)
 
             await self.session.flush()
-            for new_instance in new_instances:
-                await self.session.refresh(new_instance)
-                new_instance.pod_name = f"instance-{new_instance.simulation_id}-{new_instance.id}"
-                self.session.add(new_instance)
+            for instance in new_instances:
+                await self.session.refresh(instance)
+                instance.pod_name = await pod_service.create_pod(instance, template)
+                self.session.add(instance)
 
             return [
                 InstanceCreateResponse(
@@ -57,87 +63,56 @@ class InstanceService:
                 for new_instance in new_instances
             ]
 
-    async def create_pod(self, instance_id, instance_create_data):
-        statement = select(Template).where(Template.template_id == instance_create_data.template_id)
-        template = await self.session.scalar(statement)
+    async def get_all_instances(self, request: InstanceListRequest):
+        simulation_id = request.simulation_id
 
-        pod_client = client.CoreV1Api()
-        for i in range(instance_create_data.instance_count):
-            pod_name = f"instance-{instance_id}-{i + 1}"
-            pod_metadata = client.V1ObjectMeta(name=pod_name)
-            pod_env = client.V1EnvVar(name="AGENT_TYPE", value=template.type)
-
-            container = client.V1Container(
-                name=pod_name,
-                image="shis1008/pod:latest",
-                env=pod_env,
+        if simulation_id is None:
+            result = await self.session.execute(select(Instance))
+        else:
+            simulation = await self.simulation_service.find_simulation_by_id(simulation_id, "시뮬레이션의 인스턴스 목록 조회")
+            query = (
+                select(Instance)
+                .where(Instance.simulation_id == simulation.id)
+                .order_by(Instance.id.desc())
             )
-            pod = client.V1Pod(
-                metadata=pod_metadata,
-                spec=client.V1PodSpec(containers=[container]),
+            result = await self.session.execute(query)
+
+        instances = result.scalars().all()
+        instance_list = []
+
+        for instance in instances:
+            pod_name = instance.pod_name
+            pod_namespace = instance.pod_namespace
+
+            response = InstanceListResponse(
+                instance_id=instance.id,
+                instance_name=instance.name,
+                instance_description=instance.description,
+                instance_created_at=str(instance.created_at),
+                pod_name=pod_name,
+                pod_namespace=pod_namespace,
+                pod_status=await pod_service.get_pod_status(pod_name, pod_namespace),
             )
-
-            pod_client.create_namespaced_pod(namespace="robot", body=pod)
-
-    async def get_all_instances(self, simulation_id: Optional[int]):
-        try:
-            statement = (
-                select(Instance).
-                order_by(Instance.id.desc())
-            )
-            if simulation_id is not None:
-                statement = statement.where(Instance.simulation_id == simulation_id)
-            results = await self.session.scalars(statement)
-
-            instance_list = [
-                InstanceListResponse(
-                    instance_id=instance.id,
-                    instance_name=instance.name,
-                    instance_description=instance.description,
-                    instance_created_at=str(instance.created_at),
-                    pod_name=instance.pod_name,
-                    pod_status="RUNNING" #TODO: 실제 상태 연동
-                )
-                for instance in results.all()
-            ]
-
-        except Exception as e:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail='인스턴스 목록 조회 실패: ' + str(e))
+            instance_list.append(response)
 
         return instance_list
 
     async def get_instance(self, instance_id: int):
-        #TODO: 실제 상태 연동 및 예외 처리
         instance = await self.find_instance_by_id(instance_id, '인스턴스 상세 조회')
+        pod_name = instance.pod_name
+        namespace = instance.pod_namespace
 
-        instance_detail_response = InstanceDetailResponse(
+        return InstanceDetailResponse(
             instance_id=instance.id,
-            instance_namespace="robot",
-            instance_port_number=3000,
-            instance_age="20d",
+            pod_name=pod_name,
+            instance_namespace=namespace,
+            instance_status=await pod_service.get_pod_status(pod_name, namespace),
+            instance_image=await pod_service.get_pod_image(pod_name, namespace),
+            instance_age=await pod_service.get_pod_age(pod_name, namespace),
+            instance_label=await pod_service.get_pod_label(pod_name, namespace),
             template_type=instance.template.type,
-            instance_volume="instanceVolume",
-            instance_status="instanceStatus",
             topics=instance.template.topics,
-        ).model_dump()
-
-        return instance_detail_response
-
-    async def control_instance(self, instance_control_data: InstanceControlRequest):
-        # 추후 연동 시 로직 추가
-        instance_id = instance_control_data.instance_id
-        action = instance_control_data.action
-
-        return InstanceControlResponse(
-            instance_id=instance_control_data.instance_id
-        ).model_dump(), action
-
-    async def delete_instance(self, instance_id: int):
-        # 추후 연동 시 수동 데이터 수정
-
-        return InstanceDeleteResponse(
-            instance_id=instance_id
-        ).model_dump()
+        )
 
     async def find_instance_by_id(self, instance_id: int, api: str):
         try:
@@ -150,7 +125,8 @@ class InstanceService:
             instance = result.scalar_one_or_none()
 
         except Exception as e:
-            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f'{api} 실패 : 데이터베이스 조회 중 오류가 발생했습니다. : {str(e)}')
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=f'{api} 실패 : 데이터베이스 조회 중 오류가 발생했습니다. : {str(e)}')
 
         if instance is None:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'{api} 실패 : 존재하지 않는 인스턴스id 입니다.')
