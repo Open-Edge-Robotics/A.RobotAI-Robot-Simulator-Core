1551704c94079822813022e97a770b449f2f83dd
seo-young Yoon <ytjdud01@kookmin.ac.kr>
2024-12-04 09:58:12 +0900

    chore: [simulation] NOTHING의 값을 -- 에서 Empty로 수정

--- src/utils/my_enum.py
+++ src/utils/my_enum.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class SimulationStatus(Enum):
-    NOTHING = "--" # 시뮬레이션에 속한 인스턴스가 없는 경우
+    NOTHING = "Empty" # 시뮬레이션에 속한 인스턴스가 없는 경우
     RUNNING = "Running" # 시뮬레이션에 속한 모든 인스턴스의 pod status가 Running인 경우
 
 class PodStatus(Enum):
