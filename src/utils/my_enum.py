4052031d7114bc1a3ab97e922ed009dc3c2be75d
이노그리드 신이수 <isu010614@naver.com>
2024-12-10 10:21:33 +0900

    feat: [instance] 실행 관련 enum 추가

--- src/utils/my_enum.py
+++ src/utils/my_enum.py
@@ -1,17 +1,22 @@
 from enum import Enum
 
+
 class SimulationStatus(Enum):
-    EMPTY = "Empty" # 시뮬레이션에 속한 인스턴스가 없는 경우
-    ACTIVE = "Active" # 시뮬레이션에 속한 모든 인스턴스의 pod status가 Running인 경우
-    INACTIVE = "Inactive" # 인스턴스의 pod status가 하나라도 Running이 아닌 경우
+    EMPTY = "Empty"  # 시뮬레이션에 속한 인스턴스가 없는 경우
+    ACTIVE = "Active"  # 시뮬레이션에 속한 모든 인스턴스의 pod status가 Running인 경우
+    INACTIVE = "Inactive"  # 인스턴스의 pod status가 하나라도 Running이 아닌 경우
+
 
 class PodStatus(Enum):
     """Pod의 실제 status"""
     RUNNING = "Running"
+    STOPPED = "Stopped"
+
 
 class InstanceStatus(Enum):
     """Pod status를 Instance status로 변환"""
     READY = "Ready"
+
 
 class API(Enum):
     # 템플릿
@@ -26,9 +31,10 @@ class API(Enum):
     CONTROL_INSTANCE = "인스턴스 실행/중지"
     RUN_INSTANCE = "인스턴스 실행"
     STOP_INSTANCE = "인스턴스 실행 중지"
+    CHECK_INSTANCE = "인스턴스 실행 상태 체크"
     DELETE_INSTANCE = "인스턴스 삭제"
 
-    #시뮬레이션
+    # 시뮬레이션
     GET_SIMULATIONS = "시뮬레이션 목록 조회"
     CREATE_SIMULATION = "시뮬레이션 생성"
     CONTROL_SIMULATION = "시뮬레이션 실행/중지"
